8538ce6994f06dcf5d754656999668c1fce0a9f6
ShawnNian <s21568077@163.com>
2019-03-26 11:18:06 +0800

    初始化工程

---
+++ GItTest/GItTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GItTest
+//
+//  Created by Nian Shawn on 2019/3/26.
+//  Copyright © 2019 Nian Shawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GItTest/GItTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GItTest
+//
+//  Created by Nian Shawn on 2019/3/26.
+//  Copyright © 2019 Nian Shawn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
